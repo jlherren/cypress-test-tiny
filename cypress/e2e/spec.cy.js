d5f00bc78ebbdbe7251801f99ff1d14cb251e709
Jean-Luc Herren <jean-luc.herren@andeo.ch>
2023-05-23 09:33:46 +0200

    Add test to demonstrate #16691
    V2.

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,8 @@
+describe('page', () => {
+    it('works', () => {
+        cy.visit('https://jlh.ch/cypress/');
+        for (let i = 0; i < 25; i++) {
+            cy.contains('Save').click();
+        }
+    });
+});
